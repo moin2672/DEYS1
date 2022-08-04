4f6f5fe891e72dd4278ec50de8dd66c13ce55c3d
moin2672 <moin2672@gmail.com>
2022-08-05 00:01:16 +0530

    20. adding ckeditor

--- ravez/settings.py
+++ ravez/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     #apps
     'zblog',
     'members',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
--- zblog/models.py
+++ zblog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
-
+from ckeditor.fields import RichTextField
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -17,7 +17,8 @@ class Post(models.Model):
     title = models.CharField(max_length=600)
     title_tag = models.CharField(max_length=600) #,default="DEYS RAVEZ!"
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body=models.TextField()
+    # body=models.TextField()
+    body = RichTextField(blank=True, null=True)
     updated_date = models.DateTimeField(auto_now_add=True)
     category = models.CharField(max_length=255, default='coding')
     likes = models.ManyToManyField(User, related_name='blog_posts')
